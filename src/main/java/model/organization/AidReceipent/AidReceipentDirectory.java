338463858031c8fd8d3581eda655126109531da3
Rugved-142 <gundawar.r@northeastern.edu>
2024-12-05 12:54:38 -0500

    added aidreceipent package

---
+++ src/main/java/model/organization/AidReceipent/AidReceipentDirectory.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.organization.AidReceipent;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rugvedgundawar
+ */
+public class AidReceipentDirectory {
+    
+    String name;
+    String email;
+    String address;
+    ArrayList<AidRequest> aidHistory;
+}
